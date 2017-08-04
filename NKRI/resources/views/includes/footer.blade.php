f236a671672de338e8aae5cc245db526e6ccc18c
synelnoor <synelnoor@gmail.com>
2017-08-04 14:02:48 +0700

    NKRI weblara

---
+++ NKRI/resources/views/includes/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+  <div class="container">
+    <span class="text-muted"> Copyright &copy; Nyeruput Kopi Rasa Indonesia 2017 </span>
+  </div>
+</footer>
---
+++ NKRI/resources/views/layouts/master.blade.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title>Latihan</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+	
+</head>
+<body>
+	@include('includes.navbar')
+
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+			
+				@yield('content')
+			</div>	
+		</div>
+	</div>
+
+	@include('includes.footer')
+
+	<script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
+</body>
+</html>
+
